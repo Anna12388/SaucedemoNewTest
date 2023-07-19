1168b9c2200c9ddbc24199c8e785e8479b422331
Anna12388 <125131848+Anna12388@users.noreply.github.com>
2023-07-19 16:18:44 +0300

    screenshot

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -1,9 +1,11 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class BasePage {
+public abstract class BasePage {
     WebDriver driver;
     WebDriverWait wait;
 
@@ -12,5 +14,15 @@ public class BasePage {
     public BasePage(WebDriver driver) {
 
         this.driver = driver;
+        wait = new WebDriverWait(driver,5);
+    }
+    public abstract boolean isPageOpen();
+
+    public boolean isExist(By locator){
+        try {
+            return driver.findElement(locator).isDisplayed();
+        }catch (NoSuchElementException e) {
+            return false;
+        }
     }
 }
--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -2,8 +2,12 @@ package tests;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 import pages.InventoryPage;
 import pages.LoginPage;
 
@@ -14,21 +18,29 @@ public class BaseTest {
     LoginPage loginPage;
     InventoryPage inventoryPage;
 
+
+    @Parameters({"browser"})
     @BeforeMethod
-    public void setUp() {
+    public void setUp(String browser) {
+    if(browser.equals("chrome")){
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+        options.addArguments("--incognito");
+        driver = new ChromeDriver(options);
+    }else {
+        WebDriverManager.edgedriver().setup();
+        driver = new EdgeDriver();
+    }
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+    driver.manage().window().maximize();
 
-        LoginPage LoginPage = new LoginPage(driver);
-        InventoryPage InventoryPage = new InventoryPage(driver);
-
+        loginPage = new LoginPage(driver);
+        inventoryPage = new InventoryPage(driver);
     }
     @AfterMethod(alwaysRun = true)
     public void tearDown() {
         driver.quit();
-
     }
 }
 
